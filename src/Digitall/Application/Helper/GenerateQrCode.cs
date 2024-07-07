5ea960ceb1a5638cbf41978a43ff04a9c6cee9c2
Hossein <h.faraji8079@gmail.com>
2024-07-07 09:53:38 +0330

    ada

--- src/Digitall/Application/Helper/GenerateQrCode.cs
+++ src/Digitall/Application/Helper/GenerateQrCode.cs
@@ -1,5 +1,6 @@
 ﻿using System.Drawing.Imaging;
 using QRCoder;
+using SixLabors.ImageSharp.Formats.Qoi;
 
 namespace Application.Helper;
 
@@ -7,17 +8,24 @@ public static class GenerateQrCode
 {
     public static async Task<byte[]> GetQrCodeAsync(string text)
     {
-        // Generate QR code
-        using var qrGenerator = new QRCodeGenerator();
-        using var qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
-        using var qrCode = new QRCode(qrCodeData);
-        using var qrCodeImage = qrCode.GetGraphic(20);
+        try
+        {
+            using var qrGenerator = new QRCodeGenerator();
+            using var qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+            using var qrCode = new QRCode(qrCodeData);
+            using var qrCodeImage = qrCode.GetGraphic(20);
 
-        // Save QR code image to memory stream
-        using var ms = new MemoryStream();
-        qrCodeImage.Save(ms, ImageFormat.Png);
-        ms.Position = 0;
-        // Return image as byte array
-        return ms.ToArray();
+            // Save QR code image to memory stream
+            using var ms = new MemoryStream();
+            qrCodeImage.Save(ms, ImageFormat.Png);
+            ms.Position = 0;
+            // Return image as byte array
+            return ms.ToArray();
+        }
+        catch (System.Exception qr)
+        {
+            throw new ApplicationException("qr Kiry" + qr.Message);
+        }
+        // Generate QR code
     }
 }
