d749c8b87f23b5791de26fa10735b734e0c32891
Hossein Faraji <76825182+hosseinfaraji8079@users.noreply.github.com>
2024-12-24 09:24:38 +0330

    update agnet 100000 for added agent

--- src/digitall/Application/Services/Implementation/Agent/AgentService.cs
+++ src/digitall/Application/Services/Implementation/Agent/AgentService.cs
@@ -609,7 +609,7 @@ public class AgentService(
 
             User? user = await userRepository.GetEntityById(newAgent.AgentAdminId);
 
-            if (user.Balance <= 100000)
+            if (user.Balance < 100000)
                 throw new AppException(
                     $"موجودی کاربر برای نمایندگی کافی نیست لطفا کاربر باید {100000:N0} هزار تومن حداقل موجودی داشته باشد");
             
